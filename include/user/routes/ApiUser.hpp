0355f8bc14f722c0dd5ec0d82bc682a52ca32cdb
Gurkan Taner <gurkan.taner@epitech.eu>
2024-12-17 17:07:14 +0100

    dev: add parser.cpp to have a custom swagger and update routes

--- include/user/routes/ApiUser.hpp
+++ include/user/routes/ApiUser.hpp
@@ -12,8 +12,12 @@ using namespace std;
 
 void setup_user_routes(crow::App<ApiMiddleware, crow::CORSHandler> &app, DatabaseManager& database_manager) {
     /*
-        route: /api/users
-        method: GET
+        @Doc
+        @Route("/api/users/list")
+        @Name("Users list")
+        @Description("Get registered users list")
+        @Method(GET)
+        @Header(Authorization: Bearer token)
     */
     CROW_ROUTE(app, "/api/users/list").CROW_MIDDLEWARES(app, ApiMiddleware)([&database_manager]()
     {
@@ -30,9 +34,12 @@ void setup_user_routes(crow::App<ApiMiddleware, crow::CORSHandler> &app, Databas
         return crow::response(RESPONSE_OK, json_resp);
     });
     /*
-        route: /api/users
-        method: GET
-        protected by token
+        @Doc
+        @Route("/api/users")
+        @Name("User information")
+        @Description("Get logged user information")
+        @Method(GET)
+        @Header(Authorization: Bearer token)
     */
     CROW_ROUTE(app, "/api/users").CROW_MIDDLEWARES(app, ApiMiddleware)([&database_manager, &app](const crow::request &req)
     {
@@ -49,14 +56,17 @@ void setup_user_routes(crow::App<ApiMiddleware, crow::CORSHandler> &app, Databas
         return crow::response(RESPONSE_OK, json_resp);
     });
     /*
-        route: /api/users
-        method: PUT
-        protected by token
-        body:
-            string last_name
-            string first_name
-            string email
-            string username
+        @Doc
+        @Route("/api/users")
+        @Name("User update")
+        @Description("Update user information")
+        @Method(PUT)
+        @Header(Authorization: Bearer token)
+        @Header(Content-Type: application/json)
+        @Body(string last_name)
+        @Body(string first_name)
+        @Body(string email)
+        @Body(string username)
     */
     CROW_ROUTE(app, "/api/users").CROW_MIDDLEWARES(app, ApiMiddleware).methods(crow::HTTPMethod::Put)([&database_manager, &app](const crow::request &req)
     {
